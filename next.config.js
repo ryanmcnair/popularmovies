7489635cce2d1e82a348b683b86fd621d8ced189
ryanmcnair <ryan.mcnair@gmail.com>
2023-03-28 14:16:35 -0500

    trying another thing

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: {
-    FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    DATABASE_URL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: String(process.env.NEXT_PUBLIC_FIREBASE_API_KEY),
+    authDomain: String(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
+    projectId: String(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID),
+    storageBucket: String(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET),
+    messagingSenderId: String(process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+    appId: String(process.env.NEXT_PUBLIC_FIREBASE_APP_ID),
+    databaseURL: String(process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL),
   },
   reactStrictMode: true,
   // I don't want it to run when compiling as I trust the CI stage of the pipeline and Husky.
--- utils/client.js
+++ utils/client.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const clientCredentials = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  apiKey: String(process.env.NEXT_PUBLIC_FIREBASE_API_KEY),
+  authDomain: String(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
+  projectId: String(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID),
+  storageBucket: String(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET),
+  messagingSenderId: String(process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+  appId: String(process.env.NEXT_PUBLIC_FIREBASE_APP_ID),
+  databaseURL: String(process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL),
 };
 
 if (!firebase.apps.length) {
