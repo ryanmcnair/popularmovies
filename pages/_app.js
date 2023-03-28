a3173c926f41fa9d88a6ca3f8b266571aff24930
ryanmcnair <ryan.mcnair@gmail.com>
2023-03-28 14:01:40 -0500

    another update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import '../utils/client';
 import { AuthProvider } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 import ViewDirectorBasedOnUserAuthStatus from '../utils/ViewDirector'; // TODO: COMMENT IN FOR AUTH
 
