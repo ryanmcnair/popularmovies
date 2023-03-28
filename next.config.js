7750b5b931ea58eedd1bddc68b83e2769a6ec023
ryanmcnair <ryan.mcnair@gmail.com>
2023-03-28 13:45:56 -0500

    config update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  env: {
+    FIREBASE_API_KEY: process.env.apiKey,
+    AUTH_DOMAIN: process.env.authDomain,
+    PROJECT_ID: process.env.projectId,
+    STORAGE_BUCKET: process.env.storageBucket,
+    MESSAGING_SENDER_ID: process.env.messagingSenderId,
+    APP_ID: process.env.appId,
+  },
   reactStrictMode: true,
   // I don't want it to run when compiling as I trust the CI stage of the pipeline and Husky.
   ignoreDuringBuilds: true,
