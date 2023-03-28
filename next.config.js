8213c43198bfb535f971b73b273d709dba112eb9
ryanmcnair <ryan.mcnair@gmail.com>
2023-03-28 13:48:58 -0500

    config update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   env: {
-    FIREBASE_API_KEY: process.env.apiKey,
-    AUTH_DOMAIN: process.env.authDomain,
-    PROJECT_ID: process.env.projectId,
-    STORAGE_BUCKET: process.env.storageBucket,
-    MESSAGING_SENDER_ID: process.env.messagingSenderId,
-    APP_ID: process.env.appId,
+    FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    DATABASE_URL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   },
   reactStrictMode: true,
   // I don't want it to run when compiling as I trust the CI stage of the pipeline and Husky.
-  ignoreDuringBuilds: true,
 };
